271ab4bd9a9a93ab2987f5f47e30d57244389bd8
SadiaGhani <sadiaghani22@gmail.com>
2021-04-24 15:29:13 +0500

    Task-1
    this task contain some string manipulation functions

---
+++ Task1/src/task1/Task1.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL PC
+ */
+public class Task1 {
+
+    
+    public static int wordCounter(String s)
+    {
+        int count = 0;
+        int size= s.length();
+        for(int i = 0; i<size; i++)
+        {
+             if( ((i>0)&&(s.charAt(i)!=' ')&&(s.charAt(i-1)==' ')) || ((s.charAt(0)!=' ')&&(i==0)))
+             {
+                 count++; 
+             }
+        }
+        return count;
+    }
+    
+    public static int vowelcounter(String s)
+    {
+        int count = 0;
+        int size= s.length();
+        for(int i = 0; i<size; i++)
+        {
+              if (s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U' || s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i'|| s.charAt(i) == 'o' || s.charAt(i) == 'u' )
+             {
+                 count++; 
+             }
+        }
+        return count;
+    }
+    
+    public static int puncCount(String s)
+    {
+        int count = 0;
+        int size= s.length();
+        for(int i = 0; i<size; i++)
+        {
+             if(s.charAt(i) == '!' || s.charAt(i) == ',' || s.charAt(i) == ';' || s.charAt(i) == '.' || s.charAt(i) == '?' ||s.charAt(i) == '-' || s.charAt(i) == ':' || s.charAt(i) == '_')
+             {
+                 count++; 
+             }
+        }
+        return count;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner obj = new Scanner(System.in);
+        String s;
+        System.out.println("Enter a String to count its words, vowels, punctuation marks");
+        s = obj.nextLine();
+        System.out.println("So, the total words in String are: " +wordCounter(s)+"\ntotal number of vowels are: " +vowelcounter(s)+"\ntotal number of punctuation marks are: " + puncCount(s));
+        
+        
+    }
+    
+}
