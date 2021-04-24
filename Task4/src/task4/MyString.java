ee590fff4e8106a9acc8df94476cc1d5a0d94b68
AabrooFatima <81793791+AabrooFatima@users.noreply.github.com>
2021-04-24 21:25:40 +0500

    Task 4
    The class mystring cannot be inherited from String.

---
+++ Task4/src/task4/MyString.java
@@ -0,0 +1,130 @@
+
+package task4;
+
+/**
+    Adding functions of string manipulation to make the job easier for user
+    Last Updated April 24, 2021
+       
+    @author Aabroo Fatima
+*/
+
+public class MyString 
+{
+    /**
+        Space in a string 
+        @param s string to be manipulate
+        @return no of spaces in the string
+    */
+    public int spaceCounter(String s)
+    {
+        int n=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==' ')
+            {
+               n++;   
+            }
+        }
+        return n;
+    }
+    /**
+        lower case alphabets in a string 
+        @param t string to be manipulate
+        @return no of lowercase alphabets in the string
+    */
+    public int SmallCounter(String t)
+    {
+        int s=0;
+       
+        for(int i=0;i<t.length();i++)
+        {
+            if(t.charAt(i)>='a' && t.charAt(i)<='z')
+                s++;
+        }
+    
+    return s;
+    }
+    /**
+        Upper case alphabets in a string 
+        @param t string to be manipulate
+        @return no of Uppercase alphabets in the string
+    */
+    public int CapitalCounter(String t)
+    {
+        int c=0;
+       
+        for(int i=0;i<t.length();i++)
+        {
+            if(t.charAt(i)>='A' && t.charAt(i)<='Z')
+                c++;
+        }
+    
+    return c;
+    }
+    /**
+        If the string starts and ends with the same character
+        @param s string to be manipulate
+        @return true if starting and ending characters are same
+    */
+    public boolean EndWithSame(String s)
+    {
+        boolean flag=false;
+        int l= s.length();
+        if( s.charAt(0)==s.charAt(l-1))
+        {
+            flag =true;
+        }
+        else
+        {
+            flag = false;
+        }
+        return flag;
+    }
+    /**
+        If the string is palindrome (reads same backward)
+        @param s string to be manipulate
+        @return true if the string is palindrome
+    */
+    public boolean isPalindrome(String s)
+    {
+        boolean flag=false;
+        int l=s.length();
+        int m=0;
+        int n=0;
+        if(l%2 !=0)
+        {
+            m= l+1;
+            m=m/2;
+            for(int i=0;i<l-m;i++)
+            {
+                n=i+1;
+                if(s.charAt(i) == s.charAt(l-n))
+                    flag=true;
+                else
+                {
+                    flag=false;
+                    break;
+                }
+                
+            }
+        }
+        return flag;
+    }
+    /**
+        How many time the char has occurred in the string
+        @param s string to be manipulate
+        @param c the character to be matched
+        @return number of times character has occurred
+    */
+    public int hasOccurred(char c, String s)
+    {
+        int o=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==c)
+                o++;
+        }
+        return o;
+    }
+}    
+
---
+++ Task4/src/task4/Task4.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author A R F T
+ */
+public class Task4 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+       /* String s;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Ente string");
+          s= obj.nextLine();
+         MyString  m = new MyString();
+         System.out.println("Capital: " +m.CapitalCounter(s));
+         System.out.println("Small: " +m.SmallCounter(s));
+         System.out.println(" a has occured: " +m.hasOccurred('a', s));
+         System.out.println("Start or enf with sem: " +m.EndWithSame(s));
+         System.out.println("Palindrome: " +m.isPalindrome(s));
+         System.out.println("Spaces are : " +m.spaceCounter(s));*/
+    }
+    
+}
