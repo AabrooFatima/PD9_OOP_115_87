2d55fa4fa0562a1380705d99dbc60806b881182e
SadiaGhani <sadiaghani22@gmail.com>
2021-04-25 14:20:05 +0500

    Revert "Update Task 3"
    This reverts commit 1b9293530130e9a4439e63817bc958c0f8b39fac.

--- Task3/src/task3/Task3.java
+++ Task3/src/task3/Task3.java
@@ -22,9 +22,7 @@ public class Task3 {
      */
   public static String ridMultipleBlank(String s)
   {
-      String s2 = "";
-     s2 = s.replaceAll("\\s+", " ");
-     
+     String s2 = s.replaceAll("\\s+", " ");
      return s2;
   }
   /**
